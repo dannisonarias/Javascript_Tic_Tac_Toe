2abb263290e80b7f523edbb6f183b454ac08253e
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-06-02 15:19:04 -0500

    Setup gameboard and create game module

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,24 @@
-const playerFactory = (name) =>{
+const playerFactory = (name) => {
     name = name;
-    return {name};
+    return { name };
 }
 
+const gameBoard = (() => {
+    const board = ['', '', '', '', '', '', '', '', ''];
+
+})()
+
+const Game = (() => {
+
+    getPlayers = () => {
+        const player1 = document.getElementById('player1');
+        const player2 = document.querySelector('.board-wrapper');
+
+        return player2
+    }
+
+    return { getPlayers }
+})();
+
+
+console.log(Game.getPlayers())
