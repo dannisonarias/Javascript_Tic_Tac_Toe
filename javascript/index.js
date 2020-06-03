3f13c6788609695577d3038806f7a5d05fe186a7
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-06-03 09:54:32 -0500

    Update playerfactory, board win_combos and start game module

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,51 @@
-const playerFactory = (name) => {
-    name = name;
-    return { name };
+const playerFactory = (playerName, playerSymbol) => {
+  const name = playerName;
+  const symbol = playerSymbol;
+  const wins = 0;
+  return { name, symbol, wins };
 }
 
-const gameBoard = (() => {
-    const board = ['', '', '', '', '', '', '', '', ''];
+const display = (() => {
+  // get cells
+  const cells = document.querySelectorAll('.position');
 
+  // Get player names
+  const name1 = document.querySelector('#player1').value;
+  const name2 = document.querySelector('#player2').value;
+
+  return {
+    cells,
+    name1, name2
+  }
+
+})();
+
+const gameBoard = (() => {
+  const WIN_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+  return { WIN_COMBOS }
 })()
 
 const Game = (() => {
+  let currentPlayer = true;
 
-    getPlayers = () => {
-        const player1 = document.getElementById('player1');
-        const player2 = document.querySelector('.board-wrapper');
+  getPlayers = () => {
+    const player1 = playerFactory(displayController.name1, '&#10007;');
+    const player2 = playerFactory(displayController.name2, '&#120420;');
 
-        return player2
-    }
+    return { player1, player2 }
+  }
 
-    return { getPlayers }
+  return { getPlayers }
 })();
 
 
