d46368d5e6c70c48b6f4416274a9e3f925a9298f
DannisonArias <dannisonarias@gmail.com>
2020-06-18 13:23:06 -0400

    Add board test and playerFactory test

---
+++ src/board.test.js
@@ -0,0 +1,6 @@
+import gameBoard from './board';
+
+test("test game board array",()=>{
+    expect(gameBoard.WINS).toEqual([[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5,
+        8], [0, 4, 8], [6, 4, 2]])
+})
